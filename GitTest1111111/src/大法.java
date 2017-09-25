9ce03e8b6b56d84568f358ad540807e081a1dcbb
xjs <william_xjs@163.com>
2017-09-25 20:46:40 +0800

    便更好

--- GitTest1111111/src/大法.java
+++ GitTest1111111/src/大法.java
@@ -1,6 +1,8 @@
 
 public class 大法 {
 	public static void main(String[] args) {
-		System.out.println(11111);
-	}
+	System.out.println(11111);
+	System.out.println(11111);
+	System.out.println(11111);
+}
 }
