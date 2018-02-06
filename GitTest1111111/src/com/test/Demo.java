73dcfc4e158cc354ca938a6c514db6190e91a47f
willliamxjs <490841975@qq.com>
2018-02-06 18:15:59 +0800

    资源库同步

--- GitTest1111111/src/com/test/Demo.java
+++ GitTest1111111/src/com/test/Demo.java
@@ -2,9 +2,9 @@ package com.test;
 
 public class Demo {
 	public static void main(String[] args) {
-	System.out.println(11111);
-	System.out.println(11111);
-	System.out.println(11111);
-	System.out.println(11111);
+	System.out.println(22);
+	System.out.println(2);
+	System.out.println(111211);
+	System.out.println(112111);
 }
 }
