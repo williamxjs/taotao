ca98933b7cc8d6a1cf9fe9d58614e30e7c7a40cd
willliamxjs <490841975@qq.com>
2018-02-06 18:01:25 +0800

    测试冲突？

--- GitTest1111111/src/大法.java
+++ GitTest1111111/src/大法.java
@@ -1,9 +1,9 @@
 
 public class 大法 {
 	public static void main(String[] args) {
-	System.out.println(11111);
-	System.out.println(11111);
-	System.out.println(11111);
-	System.out.println(11111);
-}
+		System.out.println(11111);
+		System.out.println(11111);
+		System.out.println(11111);
+		System.out.println(11111);
+	}
 }
